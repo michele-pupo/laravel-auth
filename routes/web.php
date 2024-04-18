b3af1dceee065ad610ea63cc29495655b5d25fa4
michele-fabio-pupo <mikypupo@hotmail.it>
2024-04-18 15:39:20 +0200

    created the admin controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// rotta per pagina di amministrazione
+Route::get('/admin', [DashboardController::class,'index']);
