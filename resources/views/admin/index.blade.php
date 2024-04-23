e43221b5bbe9283093d17627eb1bcdfc7d9a1ae1
michele-fabio-pupo <mikypupo@hotmail.it>
2024-04-23 09:15:29 +0200

    user welcome page stylization

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-<div class="container py-5">
+<div class="container py-5 text-center">
 
-    <h1>Pagina di amministrazione</h1>
+    <h1 class="display-3 pb-5">Pagina di amministrazione</h1>
 
-    <h2>Benvenuto {{$user->name}}</h2>
+    <h2 class="pb-5">Benvenuto <span class="text-uppercase text-danger">{{$user->name}}</span></h2>
     {{-- @dump($user) --}}
 
-    <a href="{{route('admin.projects.index')}}" class="btn btn-info">Visualizza i tuoi progetti</a>
-    <a href="{{route('admin.projects.create')}}" class="btn btn-info">Aggiungi un nuovo progetto</a>
+    <a href="{{route('admin.projects.index')}}" class="btn btn-info">Visualizza progetti</a>
+    <a href="{{route('admin.projects.create')}}" class="btn btn-success">Aggiungi nuovo progetto</a>
 
 </div>
 
